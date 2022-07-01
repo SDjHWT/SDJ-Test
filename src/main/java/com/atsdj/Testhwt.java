90bee41f0e7435abc90963e0b379e4f2c3a02cd3
hwt <630256351@qq.com>
2022-07-01 20:48:01 +0800

    second commit

--- src/main/java/com/atsdj/Testhwt.java
+++ src/main/java/com/atsdj/Testhwt.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class Testhwt {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
